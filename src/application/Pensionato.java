3a67cda883f43af26e42cb129f9f4a20a5743890
Greg Lixandrão <contato@greglixandrao.com>
2023-07-05 08:54:03 -0300

    Add unfinished Pensionato exercise

---
+++ src/application/Pensionato.java
@@ -0,0 +1,18 @@
+package application;
+
+import entities.Rent;
+
+import java.util.Scanner;
+
+public class Pensionato {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Rent[] room = new Rent[10];
+
+        System.out.println("How many rooms will be rent?");
+        int n = scan.nextInt();
+
+
+    }
+}
