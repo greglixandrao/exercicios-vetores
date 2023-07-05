97d635744c4310d7facdcb091e44f9ad0b318bd4
Greg Lixandrão <contato@greglixandrao.com>
2023-07-05 08:53:31 -0300

    Add Soma_vetor.java exercise

---
+++ src/Soma_vetor.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Soma_vetor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quantos números você vai digitar?");
+        int n = scan.nextInt();
+
+        double[] numeros = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um número: ");
+            numeros[i] = scan.nextDouble();
+        }
+
+        System.out.print("VALORES = ");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.1f ", numeros[i]);
+        }
+        System.out.println();
+        double soma = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            soma += numeros[i];
+        }
+
+        double media = soma / numeros.length;
+
+        System.out.printf("SOMA = %.2f%n", soma);
+        System.out.printf("MEDIA = %.2f%n", media);
+    }
+}
